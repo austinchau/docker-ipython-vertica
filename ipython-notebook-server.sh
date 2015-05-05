95ac3e8f574e74376e3cc0e3afe3e76d86bbdc29
austin chau <austin.chau@gmail.com>
2015-05-05 13:13:42 -0700

    volume mapping

--- ipython-notebook-server.sh
+++ ipython-notebook-server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -it -v $HOME/ipython-data:/ipython-data -p 8888:8888 austinchau/ipython-vertica
+IPYTHON_DATA_HOME=$HOME/ipython-data
+mkdir -p $IPYTHON_DATA_HOME
+docker run -it -v $IPYTHON_DATA_HOME:/ipython-data -p 8888:8888 austinchau/ipython-vertica
